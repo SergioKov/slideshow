92987ec2e1da63ae21acdc4072a8b8586ac05d0f
sergio <serhiy_kovalchuk@race.es>
2024-09-30 17:03:41 +0200

    experiment con include - 1

---
+++ slide_body.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Slide Template</title>
+    <link rel="icon" type="image/png" href="./images/slideshow3.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    <link id="estilos_base" rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<div class="slide_body">
+    <div class="slide_body_inner">
+
+        
+<?php 
+    include './temas/tema2/slide_0.html';
+?>
+
+
+        
+
+
+
+
+    </div>
+</div>
+
+
+</body>
+</html>
